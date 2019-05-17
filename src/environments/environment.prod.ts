3cf26089b3a69af8aa747d5bf0703185c776af55
darekl125 <44176900+darekl125@users.noreply.github.com>
2019-05-17 06:55:23 +0200

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDO1W0CNBxbpP8qwO7fgDGg_gLQ8247gTY",
+    authDomain: "login-test-58df5.firebaseapp.com",
+    databaseURL: "https://login-test-58df5.firebaseio.com",
+    projectId: "login-test-58df5",
+    storageBucket: "login-test-58df5.appspot.com",
+    messagingSenderId: "817195632401",
+    appId: "1:817195632401:web:245df21381ec6fea"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDO1W0CNBxbpP8qwO7fgDGg_gLQ8247gTY",
+    authDomain: "login-test-58df5.firebaseapp.com",
+    databaseURL: "https://login-test-58df5.firebaseio.com",
+    projectId: "login-test-58df5",
+    storageBucket: "login-test-58df5.appspot.com",
+    messagingSenderId: "817195632401",
+    appId: "1:817195632401:web:245df21381ec6fea"
+  }
 };
 
 /*
