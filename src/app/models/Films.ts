199deb1ee557e55f3829767be33b543fa7abbb4a
MateuszSalapatek <43645706+MateuszSalapatek@users.noreply.github.com>
2019-05-17 23:17:39 +0200

    adding all attribute for Film interface

--- src/app/models/Films.ts
+++ src/app/models/Films.ts
@@ -1,5 +1,11 @@
 export interface Film {
     id?:string;
+    imgURL?:string;
     tittle?:string;
+    originalTittle?:string;
     score?:string;
+    time?:string;
+    year?:string;
+    genres?:string[];
+    countries?:string[];
   }
