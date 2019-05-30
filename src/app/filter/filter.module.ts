1fa95128dd77c676521a4d1d6713084c2eaf8734
Piotr <piotr.hereda@fideltronik.com>
2019-05-31 00:46:48 +0200

    filter and movie page front end

--- src/app/filter/filter.module.ts
+++ src/app/filter/filter.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
 
 import { IonicModule } from '@ionic/angular';
 
